20471b89d43cc375f0423a0097df937183f206eb
Ivylucyawuor <awuorivy09>
2024-07-13 18:36:23 +0300

    Modifying elements test

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+// removing DOM node main
+document.getElementById('main').remove();
+//has a newHeader variable that points to the <h1> node
+const newHeader = document.createElement('h1');
+//newHeader variable points to the <h1> node that has an id of 'Victory'
+newHeader.id = 'victory';
+// newHeader has a text
+newHeader.textContent = 'Ivy is the champion';
+document.body.appendChild(newHeader);
